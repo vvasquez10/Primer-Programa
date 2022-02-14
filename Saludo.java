c8d23ff910a60e33b2abf61462fd35ab0b4cdb1a
Victor Vasquez <daxos10.0@gmail.com>
2022-02-14 12:00:40 -0500

    despliegue

---
+++ Saludo.java
@@ -0,0 +1,14 @@
+class Saludo{
+
+    public static void main(String args[]){
+
+        String miNombre = "Victor Vasquez";
+        int edad = 23;
+        String miCiudad = "Trujillo, Perú";
+
+        System.out.println("Mi nombre es " + miNombre);
+        System.out.println("Tengo " + edad + " años de edad");
+        System.out.println("Mi ciudad natal es " + miCiudad);
+
+    }
+}
